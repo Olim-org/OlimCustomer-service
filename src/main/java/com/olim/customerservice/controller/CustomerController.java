a7a37415bd18e23f8cc27dfc7aaad596e1b7470f
wodn1478 <wodn14789@naver.com>
2024-02-06 20:48:31 +0900

    init : Create CustomerController #4
    고객 컨트롤러 추가
    
    Github issue #4

---
+++ src/main/java/com/olim/customerservice/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package com.olim.customerservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customer-service/customer")
+
+public class CustomerController {
+}
